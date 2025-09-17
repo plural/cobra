468ac2af8481011a05668e6867028f12d5949728
Ryan Lankin <rlankin@gmail.com>
2025-09-16 23:03:39 -0600

    Added Flipper flag for custom table numbering

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
   config.after_initialize do
     Flipper.enable :allow_self_reporting
     Flipper.enable :demo_tournaments
+    Flipper.enable :custom_table_numbering
   rescue StandardError => e
     Rails.logger.warn "Failed setting Flipper features: #{e.class}"
   end
