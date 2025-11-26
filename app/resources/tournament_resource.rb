d3851115c7d7a4764f7d29e0553a8b653d6f58af
Jason Gessner <jason@multiply.org>
2025-11-26 04:06:50 +0000

    Use timestamp for registration and start time.
    Re-organize attributes.

--- app/resources/tournament_resource.rb
+++ app/resources/tournament_resource.rb
@@ -8,53 +8,66 @@ class TournamentResource < ApplicationResource
 
   attribute :id, :integer
   attribute :name, :string
-
-  attribute :created_at, :datetime
-  attribute :updated_at, :datetime
-
-  attribute :abr_code, :string
-  attribute :date, :date
   attribute :slug, :string
-
+  attribute :abr_code, :string
   attribute :private, :boolean
 
-  attribute :tournament_type_id, :integer
-  attribute :stage, :string
-
-  # TODO(plural): What is previous_id?
-  #  previous_id: nil,
-
-  # TODO(plural): Replace this with an NRDB username
   attribute :user_id, :integer
   attribute :tournament_organizer, :string do
     @object.user&.nrdb_username
   end
 
+  attribute :date, :date
+  attribute :time_zone, :string
+  attribute :registration_starts, :datetime do
+    create_timestamp(@object.date, @object.registration_starts, @object.time_zone)
+  end
+  attribute :tournament_starts, :datetime do
+    create_timestamp(@object.date, @object.tournament_starts, @object.time_zone)
+  end
+
+  attribute :organizer_contact, :string
+  attribute :event_link, :string
   attribute :stream_url, :string
+  attribute :description, :string
+  attribute :additional_prizes_description, :string
+  attribute :official_prize_kit_id, :integer
+
+  attribute :stage, :string
+
   attribute :manual_seed, :boolean
   attribute :self_registration, :boolean
   attribute :nrdb_deck_registration, :boolean
+  attribute :decklist_required, :boolean
+  attribute :allow_self_reporting, :boolean
+  attribute :allow_streaming_opt_out, :boolean
+
   attribute :all_players_unlocked, :boolean
   attribute :any_player_unlocked, :boolean
   attribute :registration_closed, :boolean
   attribute :swiss_deck_visibility, :string
   attribute :cut_deck_visibility, :string
-  attribute :allow_streaming_opt_out, :boolean
+
   attribute :swiss_format, :string
+  attribute :tournament_type_id, :integer
   attribute :format_id, :integer
   attribute :deckbuilding_restriction_id, :string
-  attribute :registration_starts, :string
-  attribute :tournament_starts, :string
-  attribute :decklist_required, :boolean
-  attribute :organizer_contact, :string
-  attribute :event_link, :string
-  attribute :description, :string
-  attribute :additional_prizes_description, :string
-  attribute :official_prize_kit_id, :integer
   attribute :card_set_id, :string
-  attribute :time_zone, :string
-  attribute :allow_self_reporting, :boolean
+
+  attribute :created_at, :datetime
+  attribute :updated_at, :datetime
 
   belongs_to :tournament_type
   belongs_to :user
+
+  def create_timestamp(date, time_24h, timezone)
+    # Parse the date and time string
+    datetime_string = "#{date} #{time_24h}"
+
+    # Create a Time object in the specified timezone
+    Time.zone = timezone
+    Time.zone.parse(datetime_string)
+  rescue ArgumentError => e
+    nil
+  end
 end
