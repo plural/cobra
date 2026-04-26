8002532eb073a9e95445dcdbe5395d1d7dc9dec2
Jason Gessner <jason@multiply.org>
2026-04-26 07:21:45 +0000

    Update remaining config files from Rails 8.1 upgrade.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
-  # Show full error reports.
+  # Show full error reports and disable caching.
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
