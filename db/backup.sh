ed85acfd7933ecfbe6bdd57a88d8da49accb786a
Jason Gessner <jason@multiply.org>
2026-05-03 12:34:13 -0500

    Update db backup and restore commands to use new docker setup.

--- db/backup.sh
+++ db/backup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -e
+set -u
+
 OUT_DIR=$1
 
 # Make a database backup in the specified $OUT_DIR
 
-docker compose -f docker-compose.yml -f docker-compose.prod.yml \
+docker compose \
   exec db pg_dumpall -c -U postgres \
   | gzip -9 \
   > "${OUT_DIR}"/cobra-db-dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql.gz
