1ea70e61b7f202a81098ffb9cd35eb1e726212fe
jason gessner <jason@multiply.org>
2026-08-02 21:22:19 +0000

    Add basic test to ensure the route renders.

---
+++ frontend/src/routes/help/help.svelte.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/svelte";
+import HelpPage from "./+page.svelte";
+
+describe("Help Page", () => {
+  it("renders properly", () => {
+    const { getByRole } = render(HelpPage);
+
+    const heading = getByRole("heading", { level: 1, name: "How to use Cobra" });
+    expect(heading).toBeDefined();
+  });
+});
