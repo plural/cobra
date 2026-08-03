f47d7ad97f96adcca8494aff687eae76025654cd
jason gessner <jason@multiply.org>
2026-08-03 22:08:52 +0000

    Copy over shared components and support library into frontend/

---
+++ frontend/src/lib/components/ModalDialog.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import FontAwesomeIcon from "./FontAwesomeIcon.svelte";
+
+  interface Props {
+    id: string;
+    headerText: string;
+    children?: Snippet;
+    footer?: Snippet;
+  }
+
+  let { id, headerText, children, footer }: Props = $props();
+
+  function portal(node: HTMLElement) {
+    document.body.appendChild(node);
+    return {
+      destroy() {
+        if (node.parentNode) {
+          node.parentNode.removeChild(node);
+        }
+      }
+    };
+  }
+</script>
+
+<div {id} class="modal fade" role="dialog" tabindex="-1" aria-hidden="true" use:portal>
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4>{headerText}</h4>
+        <button
+          type="button"
+          class="close"
+          data-dismiss="modal"
+          aria-label="Close"
+        >
+          <FontAwesomeIcon icon="times" />
+        </button>
+      </div>
+
+      <div class="modal-body">
+        {@render children?.()}
+      </div>
+
+      {#if footer}
+        <div class="modal-footer">
+          {@render footer()}
+        </div>
+      {/if}
+    </div>
+  </div>
+</div>
