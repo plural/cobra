fcf3b797ba72cd7704f669081bfef9241e929f01
jason gessner <jason@multiply.org>
2026-08-07 19:16:50 +0000

    Add test for separate side bias calcs by stage.

--- spec/services/side_determiner_spec.rb
+++ spec/services/side_determiner_spec.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.describe SideDeterminer do
-  let(:stage) { create(:stage) }
-  let(:decision) { described_class.determine_sides(player1, player2, stage) }
+  let(:swiss_stage) { create(:stage) }
+  let(:cut_stage) { create(:stage) }
+  let(:decision) { described_class.determine_sides(player1, player2, swiss_stage) }
   let(:player1) { create(:player) }
   let(:player2) { create(:player) }
-  let(:round) { create(:round, stage:) }
+  let(:player3) { create(:player) }
+  let(:player4) { create(:player) }
+  let(:swiss_round1) { create(:round, stage: swiss_stage) }
+  let(:cut_round1) { create(:round, stage: cut_stage) }
 
   context 'no games' do
     it 'does not set sides' do
@@ -13,10 +17,45 @@ RSpec.describe SideDeterminer do
     end
   end
 
+  context 'side bias' do
+    before do
+      # Stage 1, Round 1 side bias:
+      #   player1:   1
+      #   player2:  -1
+      #   player3:   1
+      #   player4:  -1
+      create(:pairing, player1:, player2:, side: :player1_is_corp, score1: 3, score2: 0,
+                       round: swiss_round1)
+      create(:pairing, player1: player3, player2: player4, side: :player1_is_corp, score1: 3, score2: 0,
+                       round: swiss_round1)
+
+      # Stage 2, Round 1 side bias:
+      #   player1:  -1
+      #   player2:   1
+      #   player3:  -1
+      #   player4:   1
+      create(:pairing, player1: player4, player2: player1, side: :player1_is_corp, score1: 3, score2: 0,
+                       round: cut_round1)
+      create(:pairing, player1: player2, player2: player3, side: :player1_is_corp, score1: 3, score2: 0,
+                       round: cut_round1)
+    end
+
+    it 'is calculated separately for each stage' do
+      expect(described_class.differential(player1,
+                                          swiss_stage)).not_to eq(described_class.differential(player1, cut_stage))
+      expect(described_class.differential(player2,
+                                          swiss_stage)).not_to eq(described_class.differential(player2, cut_stage))
+      expect(described_class.differential(player3,
+                                          swiss_stage)).not_to eq(described_class.differential(player3, cut_stage))
+      expect(described_class.differential(player4,
+                                          swiss_stage)).not_to eq(described_class.differential(player4, cut_stage))
+    end
+  end
+
   context 'difference' do
     before do
-      create(:pairing, player1:, side: :player1_is_corp, score1: 3, round:)
-      create(:pairing, player1: player2, side: :player1_is_runner, score1: 3, round:)
+      create(:pairing, player1:, side: :player1_is_corp, score1: 3, round: swiss_round1)
+      create(:pairing, player1: player2, side: :player1_is_runner, score1: 3, round: swiss_round1)
     end
 
     it 'determines weaker side' do
@@ -26,9 +65,9 @@ RSpec.describe SideDeterminer do
 
   context 'imbalance' do
     before do
-      create(:pairing, player1:, side: :player1_is_corp, score1: 3, round:)
-      create(:pairing, player1: player2, side: :player1_is_corp, score1: 3, round:)
-      create(:pairing, player1: player2, side: :player1_is_corp, score1: 3, round:)
+      create(:pairing, player1:, side: :player1_is_corp, score1: 3, round: swiss_round1)
+      create(:pairing, player1: player2, side: :player1_is_corp, score1: 3, round: swiss_round1)
+      create(:pairing, player1: player2, side: :player1_is_corp, score1: 3, round: swiss_round1)
     end
 
     it 'picks player who has played the overplayed side least' do
@@ -38,8 +77,8 @@ RSpec.describe SideDeterminer do
 
   context 'some games played but tied' do
     before do
-      create(:pairing, player1:, side: :player1_is_corp, score1: 3, round:)
-      create(:pairing, player1: player2, side: :player1_is_corp, score1: 3, round:)
+      create(:pairing, player1:, side: :player1_is_corp, score1: 3, round: swiss_round1)
+      create(:pairing, player1: player2, side: :player1_is_corp, score1: 3, round: swiss_round1)
     end
 
     it 'randomly picks sides' do
