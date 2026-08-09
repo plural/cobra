156e0e27a773d0014d9396a5cb95b7c895387376
Ryan Lankin <rlankin@gmail.com>
2026-08-09 08:34:04 -0600

    Added a small SvelteKit lesson comment

--- frontend/src/routes/tournaments/type/[typeId]/+page.svelte
+++ frontend/src/routes/tournaments/type/[typeId]/+page.svelte
@@ -2,6 +2,10 @@
   import type { PageProps } from "./$types";
   import TournamentList from "../../TournamentList.svelte";
 
+  // Quick SvelteKit lesson:
+  // - data is a combination of the object returned by the load() method in
+  //   +page.ts and its parents' load() objects (i.e. that of the main layout).
+  // - params are the page parameters, in this case the [typeId] URL slug.
   let { data, params }: PageProps = $props();
 </script>
 
