2677a1babd64f46386688192604d47a8cf27af7b
jason gessner <jason@multiply.org>
2026-09-04 06:34:05 +0000

    Add new test cases for when cut buttons are available.

--- spec/feature/tournaments/cut_tournament_spec.rb
+++ spec/feature/tournaments/cut_tournament_spec.rb
@@ -34,12 +34,37 @@ RSpec.describe 'cutting tournament', type: :feature do
       end
     end
 
-    context 'on rounds page' do
+    context 'on pairings page with no completed rounds' do
       before do
         visit tournament_rounds_path(tournament)
       end
 
-      it 'creates double elim stage' do
+      it 'has no cut links when no rounds have been completed' do
+        expect(page).to have_text('Swiss') # Shows a default stage.
+        expect(page).to have_no_text('Cut to Top')
+      end
+    end
+
+    context 'on pairings page with no stages' do
+      before do
+        tournament.stages.destroy_all
+        visit tournament_rounds_path(tournament)
+      end
+
+      it 'has no cut links when no rounds have been completed' do
+        expect(page).to have_text('Add Swiss stage')
+        expect(page).to have_no_text('Cut to Top')
+      end
+    end
+
+    context 'on pairings page with a completed swiss round' do
+      before do
+        create(:round, stage: tournament.stages.first, completed: true)
+
+        visit tournament_rounds_path(tournament)
+      end
+
+      it 'shows cut buttons and allows creating double elim stage' do
         expect(tournament.stages.size).to eq(1)
 
         find("a[id='double_elim_top_8']").click
@@ -48,7 +73,7 @@ RSpec.describe 'cutting tournament', type: :feature do
         expect(tournament.stages.last.format).to eq('double_elim')
       end
 
-      it 'creates single elim stage' do
+      it 'shows cut buttons and allows creating single elim stage' do
         expect(tournament.stages.size).to eq(1)
 
         find("a[id='single_elim_top_4']").click
